5a1a6eaa4589cd6b3c68c4e0fd042e8412e66b80
Richard Ma <maxiuyuan1998@gmail.com>
2021-03-22 16:11:53 -0400

    code share

--- src/redux/actions/code.js
+++ src/redux/actions/code.js
@@ -21,6 +21,44 @@ export const sendCodeToCompile = (code) => {
   };
 };
 
+export const sendCodeToShare = (key, code) => {
+  return function (dispatch) {
+    request
+      .post("write", { data: { key: key,code: code } })
+      .then((data) => {
+        dispatch({
+          type: actionTypes.response.UPDATE,
+          payload: data,
+        });
+      })
+      .catch((error) =>
+        dispatch({
+          type: "",
+          payload: error,
+        })
+      );
+  };
+};
+
+export const getCodeToShare = (key) => {
+  return function (dispatch) {
+    request
+      .get("read", { data: { key: key } })
+      .then((data) => {
+        dispatch({
+          type: actionTypes.code.NEW,
+          payload: Object.values(data).join(""),
+        });
+      })
+      .catch((error) =>
+        dispatch({
+          type: "",
+          payload: error,
+        })
+      );
+  };
+};
+
 export const updateCodeValue = (code) => {
   return {
     type: actionTypes.code.NEW,
